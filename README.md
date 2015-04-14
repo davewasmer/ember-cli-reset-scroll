7e858725e25396860436f61db2f3585f82bb7762
Dave Wasmer <davewasmer@gmail.com>
2015-04-14 18:11:10 -0400

    add mixin and update Readme

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# Ember-cli-reset-scroll
+# ember-cli-reset-scroll
 
-This README outlines the details of collaborating on this Ember addon.
+Resets the browser window scroll position when a route transitions:
+
+```js
+import Ember from 'ember';
+import ResetScrollMixin from 'ember-cli-reset-scroll';
+
+export default Ember.Route.extend(ResetScrollMixin, {
+
+  // Scrolls to top
+  resetScroll: undefined
+
+  // Scroll to a specific position (in px)
+  resetScroll: 20
+
+  // Scroll to a specific position based on the route name (in px)
+  resetScroll: {
+    'books.index': 30,
+    'authors.*': 210
+  }
+
+});
+```
 
 ## Installation
 
---
+++ addon/index.js
@@ -0,0 +1,46 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  _actions: {
+    willTransition: function(transition) {
+      this._super();
+      var resetScroll = this.get('resetScroll');
+
+      // default is opt-in since it's a mixin, set to false to explicitly opt-out
+      if (resetScroll === false) {
+        return;
+
+      // scroll to a specific position
+      } else if (Ember.typeOf(resetScroll) === 'number') {
+        scrollToPosition(resetScroll);
+
+      // scroll to a specific position based on the route name
+      } else if (Ember.typeOf(resetScroll) === 'object') {
+        resetScroll.find(function(position, routePattern) {
+          if (matches(routePattern, transition.targetName)) {
+            scrollToPosition(position);
+            return true;
+          }
+        });
+
+      // scroll to top
+      } else {
+        scrollToPosition(0);
+      }
+    }
+  }
+});
+
+function matches(pattern, routeName) {
+  var escaped = pattern
+    .replace(/\./g, '\.')
+    .replace(/\*/g, '[^\.]+');
+  var regex = new RegExp(escaped);
+  return routeName.match(regex);
+}
+
+function scrollToPosition(position) {
+  if (Ember.$(document).scrollTop() > position) {
+    window.scrollTo(0, position);
+  }
+}
