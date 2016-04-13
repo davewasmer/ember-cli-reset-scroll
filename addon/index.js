e11cfd9b8c2cbdfa588036ca59dca04b68888024
Ricardo Mendes <rokusu@gmail.com>
2016-04-13 15:06:25 +0100

    Fixes ember-runtime.action-handler-_actions deprecation

--- addon/index.js
+++ addon/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  _actions: {
+  actions: {
     willTransition: function(transition) {
       this._super();
       var resetScroll = this.get('resetScroll');
